02ef4ac0a3dd27839cbbf2ff4bb256f2d9807a79
Metalboy0 <will.v.garrison@gmail.com>
2023-05-02 11:46:38 -0500

    some changes to Config specifying some dimensions

--- Config.py
+++ Config.py
@@ -1,2 +1,7 @@
 winwidth = 700
 winheight = 700
+boarderwidth = 0
+gridwidth = 4
+gridheight = 4
+blockwidth = (winwidth-boarderwidth*5)/gridwidth
+blockheight = (winheight-boarderwidth*5)/gridheight
