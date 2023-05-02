f7b025a554a1ecc6ef3e6c3bc3e05ab73240cb64
Pyric <97044307+PyricRL@users.noreply.github.com>
2023-05-02 09:23:13 -0500

    Added class for the block

--- Main.py
+++ Main.py
@@ -4,6 +4,10 @@ pygame.init()
 
 win = pygame.display.set_mode((Config.winwidth, Config.winheight))
 
+class Block():
+    def __init__(self,number,color,width,height):
+        pass
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
