1981f4ea6d651f7e91b36c7527947a9bdfba6046
Metalboy0 <will.v.garrison@gmail.com>
2023-05-02 14:30:57 -0500

    Added some examples

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+from typing import Any
 import pygame, sys
 import Config
 
@@ -5,23 +6,29 @@ pygame.init()
 
 win = pygame.display.set_mode((Config.winwidth, Config.winheight))
 
-def createGrid():
-    """simply creates the Blocks to go on the grid and returns a group in a one liner B)"""
-    return pygame.sprite.Group([
-    [
-        Block(
-            0, 
-            (255, 0, 0),
-            Config.blockwidth,
-            Config.blockheight,
-            x * (Config.blockwidth + Config.boarderwidth)+Config.boarderwidth,
-            y * (Config.blockheight + Config.boarderwidth)+Config.boarderwidth,
-            0
-        )
-        for x in range(Config.gridwidth)
-    ]
-    for y in range(Config.gridheight)
-    ])
+
+def createGrid() -> pygame.sprite.Group:
+    """simply creates the Blocks to go on the grid and returns a group in a one-liner B)"""
+    return pygame.sprite.Group(
+        [
+            [
+                Block(
+                    0,
+                    (255, 0, 0),
+                    Config.blockwidth,
+                    Config.blockheight,
+                    x * (Config.blockwidth + Config.boarderwidth) + Config.boarderwidth,
+                    y * (Config.blockheight + Config.boarderwidth)
+                    + Config.boarderwidth,
+                    0,
+                )
+                for x in range(Config.gridwidth)
+            ]
+            for y in range(Config.gridheight)
+        ]
+    )
+
+
 class Block(pygame.sprite.Sprite):
     def __init__(self, number, color, width, height, x, y, score):
         super().__init__()
@@ -39,17 +46,17 @@ class Block(pygame.sprite.Sprite):
         self.image.fill(color)
 
     def Draw(self):
-
         win.blit(self.image, self.rect)
 
-#basically a loop through all of the game board positions, maps them to pixels,
-#then creates a Block sprite to go there
 
 blocks = createGrid()
+blocks.sprites()[0].image.fill( # example of changing the color of one of the blocks to blue
+    (0, 0, 255)
+) 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        blocks.draw(win)
+        blocks.draw(win)  # draws the sprite group of blocks to the window
         pygame.display.update()
