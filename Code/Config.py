e1c638c93d19d06115717a0347a76e9f3a423ed7
Metalboy0 <will.v.garrison@gmail.com>
2023-05-02 14:56:26 -0500

    Fixed a bug relating to only having five boarders

--- Code/Config.py
+++ Code/Config.py
@@ -3,7 +3,7 @@ winheight = 700
 boarderwidth = 4
 gridwidth = 5
 gridheight = 4
-blockwidth = (winwidth-boarderwidth*5)//gridwidth
+blockwidth = (winwidth-boarderwidth*(gridwidth+1))//gridwidth
               #700  -  4  *  5  /  5
-blockheight = (winheight-boarderwidth*5)//gridheight
+blockheight = (winheight-boarderwidth*(gridheight+1))//gridheight
                #700  -  4  *  5  /  4
